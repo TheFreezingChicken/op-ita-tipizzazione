a24bb0d3db18ac19ab43e6fb5dc90af1a1ba891f
The Freezing Chicken <thefreezingchicken+github@gmail.com>
2024-08-19 21:32:51 +0200

    Minimalizza e aggiorna

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,4 +1,18 @@
-document.querySelectorAll('.btn').forEach(button => {
+const allParalButtons = document.querySelectorAll('.paralBtn');
+const lightDarkButton = document.getElementById('lightDarkButton');
+const body = document.body;
+
+lightDarkButton.addEventListener('click', () => {
+    body.classList.toggle('dark-mode');
+    body.classList.toggle('light-mode');
+    lightDarkButton.textContent = body.classList.contains('dark-mode') ? '☀️' : '🌙';
+});
+
+// Set default mode
+body.classList.add('light-mode');
+
+
+allParalButtons.forEach(button => {
     button.addEventListener('click', function () {
         const row = this.getAttribute('data-row');
         const side = this.classList.contains('left-btn') ? 'left-btn' : 'right-btn';
